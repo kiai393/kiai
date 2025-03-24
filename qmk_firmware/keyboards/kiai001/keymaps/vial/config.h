a8279c88872dfc4ebb1d84290f9477e79c7dfd49
kiai393 <jchikaj@yahoo.co.jp>
2025-03-24 18:38:27 +0900

    Delete qmk_firmware directory

--- qmk_firmware/keyboards/kiai001/keymaps/vial/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define VIAL_KEYBOARD_UID {0x5b, 0x31, 0x4f, 0xda, 0x6f, 0x05, 0xc7, 0x62}
-#define VIAL_TAP_DANCE_ENTRIES 8
-#define VIAL_COMBO_ENTRIES 8
-#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
